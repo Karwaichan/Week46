4ab57d00268e2b143dbed0dcbcf40140f5a8b79f
Karwaichan <43789200+Karwaichan@users.noreply.github.com>
2018-11-20 08:57:16 +0100

    Add files via upload
    2e commit...

--- Toegangsysteem/src/hoofdprogramma.java
+++ Toegangsysteem/src/hoofdprogramma.java
@@ -3,11 +3,18 @@ public class hoofdprogramma {
 	public static void main(String[] args) {
 
 		Mens mens = new Mens(123);
-		Pasje pasje = new Pasje(123);
+		Pasje pasje = new Pasje(1234);
 		Toegangspoort toegang = new Toegangspoort();
 
-		toegang.geefToegang(mens, pasje);
-
+		//toegang.geefToegang(mens, pasje);
+		if(toegang.geefToegang(mens, pasje) == false) {
+			System.out.println("COMPUTER SAYS NO! -Je mag niet naar binnen!-");
+			System.out.println("pasje wordt geblokkeert");
+		}else
+			System.out.println("Goedgekeurd!");
+			System.out.println("Welkom");
+		
+		
 	}
 }
 
@@ -15,10 +22,10 @@ class Toegangspoort {
 
 	boolean geefToegang(Mens identificatie, Pasje x) {
 		if (x.pasId == identificatie.mensId) {
-			System.out.println("Je mag naar binnen");
+			//System.out.println("Je mag naar binnen");
 			return true;
 		} else
-			System.out.println("Je mag niet naar binnen");
+			//System.out.println("Je mag niet naar binnen");
 		return false;
 	}
 
@@ -39,4 +46,11 @@ class Pasje {
 	Pasje(int i) {
 		pasId = i;
 	}
+	
+//	void pasjeBlokkeren() {
+//		if(toegang.geefToegang(mens, pasje) == false) {
+//			System.out.println("pasje wordt geblokkeert");
+//		}else
+//			System.out.println("Kom binnen");
+//	}
 }
